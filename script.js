a47e6451ca7dd2b0e5e93de51bd5de6feca03213
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-06-28 11:40:16 +0000

    Continue work on logic

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 const body = document.querySelector("body");
-const gridSize = document.querySelector("input[name=pixels]").value;
+const gridSize = document.querySelectorAll("input");
 
-const WIDTH = 400;
-const HEIGHT = 400;
+const WIDTH = 640;
+const HEIGHT = 640;
 let divisor = gridSize.value;
-
-const widthPixel = WIDTH/divisor-2;
-const heightPixel = HEIGHT/divisor-2;
 
 const container = document.createElement("div");
 container.setAttribute("style", `height: ${HEIGHT}px; width: ${WIDTH}px; border: 1px solid black`);
 container.setAttribute("id", "container");
 body.appendChild(container);
-console.log(gridSize)
-
-
 
 const pixel = document.createElement("div");
 
-gridSize.addEventListener("change", () =>{
-  console.log(gridSize)
-  divisor = gridSize;
-  for(i=0; i<(divisor * divisor); i++){
-    const pixel = document.createElement("div");
-    pixel.setAttribute("style", `height: ${heightPixel}px; width: ${widthPixel}px; border: 1px solid black`);
-    container.appendChild(pixel);
-  }
-  
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+gridSize.forEach((item) =>{
+  item.addEventListener("input" , () =>{
+    divisor = item.value;
+    let widthPixel = WIDTH/divisor-2;
+    let heightPixel = HEIGHT/divisor-2;
+    removeAllChildNodes(container)
+    console.log(widthPixel)
+    
+    for(i=0; i<(divisor * divisor); i++){
+      const pixel = document.createElement("div");
+      pixel.setAttribute("style", `height: ${heightPixel}px; width: ${widthPixel}px; border: 1px solid black`);
+      pixel.style.backgroundColor = "white";
+      pixel.style.border = "1px solid hsl(50, 32%, 85%)";
+      container.appendChild(pixel);
+      
+    }
+  });
 });
 
 
-
-
-
+  
 
