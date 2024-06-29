9959e3912d9f5e199b837d4438f96629b85d34f3
boestcodes <171556185+boestcodes@users.noreply.github.com>
2024-06-29 13:17:51 +0000

    Continue working on Project

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const body = document.querySelector("body");
 const gridSize = document.querySelectorAll("input");
+const canvas = document.querySelector("#canvas");
 
-const WIDTH = 640;
-const HEIGHT = 640;
+const WIDTH = 320;
+const HEIGHT = 320;
 let divisor = gridSize.value;
 
 const container = document.createElement("div");
 container.setAttribute("style", `height: ${HEIGHT}px; width: ${WIDTH}px; border: 1px solid black`);
 container.setAttribute("id", "container");
-body.appendChild(container);
-
-const pixel = document.createElement("div");
+canvas.appendChild(container);
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -21,16 +20,19 @@ function removeAllChildNodes(parent) {
 gridSize.forEach((item) =>{
   item.addEventListener("input" , () =>{
     divisor = item.value;
-    let widthPixel = WIDTH/divisor-2;
-    let heightPixel = HEIGHT/divisor-2;
+    //let widthPixel = WIDTH/divisor-2;
+    //let heightPixel = HEIGHT/divisor-2;
+    let heightPixel = 100/divisor
+    let widthPixel = 100/divisor
     removeAllChildNodes(container)
     console.log(widthPixel)
     
     for(i=0; i<(divisor * divisor); i++){
+      console.log(i)
       const pixel = document.createElement("div");
-      pixel.setAttribute("style", `height: ${heightPixel}px; width: ${widthPixel}px; border: 1px solid black`);
-      pixel.style.backgroundColor = "white";
-      pixel.style.border = "1px solid hsl(50, 32%, 85%)";
+      pixel.setAttribute("style", `height: ${heightPixel}%; width: ${widthPixel}%`);
+      pixel.style.backgroundColor = "green";
+      //pixel.style.border = "1px solid hsl(50, 32%, 85%)";
       container.appendChild(pixel);
       
     }
@@ -39,4 +41,3 @@ gridSize.forEach((item) =>{
 
 
   
-
